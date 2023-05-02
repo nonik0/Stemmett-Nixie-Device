9a7b041a8e439f8b63fa283c8b714c750e740696
Nick Brown <nworbn@gmail.com>
2023-05-01 22:16:02 -0700

    fade fit and finish

--- lib/Animation/fadeAnimation.cpp
+++ lib/Animation/fadeAnimation.cpp
@@ -2,47 +2,82 @@
 
 #include "fadeAnimation.h"
 
-void FadeAnimation::setFade(Tube tubes[NUM_TUBES], int targetBrightness) {
+int* FadeAnimation::getRandomInitialDelays(FadePattern fadeType, int fadeMultiplier) {
+  static int initialDelays[NUM_TUBES];
+  int* randomTubeOrder = (fadeType == FadeRandom) ? getRandomTubeOrder() : NULL;
 
-  Serial.println("FadeAnimation::SetFade");
-  Serial.printf("curBrt:%3d|%3d|%3d|%3d|%3d|%3d\n",
-    tubes[5].Brightness,
-    tubes[4].Brightness,
-    tubes[3].Brightness,
-    tubes[2].Brightness,
-    tubes[1].Brightness,
-    tubes[0].Brightness);
-  Serial.printf("tarBrt:%d\n", targetBrightness);
-  Serial.flush();
-
-  int brightnessStep = 8;
   for (int i = 0; i < NUM_TUBES; i++) {
-    int brightnessDelta = targetBrightness - tubes[i].Brightness;
-    int brightnessStepCount = max(1,  (int) ceil(abs(brightnessDelta) / brightnessStep));
-    int fadeDurationMs = i * 1024;
-    int fadeStepDelayMs = fadeDurationMs / brightnessStepCount;
-
-    _tubeFadeState[i].targetBrightness = targetBrightness;
-    _tubeFadeState[i].step = brightnessDelta > 0 ? brightnessStep : -brightnessStep;
-    _tubeFadeState[i].delayReset = fadeStepDelayMs;
-    _tubeFadeState[i].delay = fadeStepDelayMs; // add initial delay for offset
-    _tubeFadeState[i].isComplete = false;
-
-    Serial.printf("%d|TB%d|S%d|D%d\n",
-      i, _tubeFadeState[i].targetBrightness, _tubeFadeState[i].step, _tubeFadeState[i].delay);
-    // Serial.printf("trBrt:%d\n", _tubeFadeState[i].targetBrightness);
-    // Serial.printf("step:%d\n", _tubeFadeState[i].step);
-    // Serial.printf("delayReset:%d\n", _tubeFadeState[i].delayReset);
-    // Serial.printf("delay:%d\n", _tubeFadeState[i].delay);
+    switch (fadeType) {
+      case FadeLeft:
+        initialDelays[i] = i * fadeMultiplier; break;
+      case FadeRight:
+        initialDelays[i] =  (NUM_TUBES - i - 1) * fadeMultiplier; break;
+      case FadeRandom:
+        initialDelays[i] =  randomTubeOrder[i] * fadeMultiplier; break;
+    }
   }
+
+  return initialDelays;
+}
+
+int* FadeAnimation::getRandomTubeOrder() {
+  static int randomTubeOrder[NUM_TUBES];
+  
+  for (int i = 0; i < NUM_TUBES; i++) {
+    randomTubeOrder[i] = i;
+  }
+  for (int i = 0; i < NUM_TUBES; i++) {
+    int randomIndex = random(0, NUM_TUBES);
+    int temp = randomTubeOrder[i];
+    randomTubeOrder[i] = randomTubeOrder[randomIndex];
+    randomTubeOrder[randomIndex] = temp;
+  }
+
+  return randomTubeOrder;
+}
+
+void FadeAnimation::setNextProgression() {
+  Serial.printf("FadeProgression:%d->", _fadeProgression);
+  _fadeProgression = (FadeProgression)((int)_fadeProgression + 1);
+  Serial.println(_fadeProgression);
+
+  int randomDelay = (_fadeProgression == FadeIn) ? random(1000) : 0;
+  switch (_fadeProgression) {
+    case FadeOut:
+      setRandomFadePattern(0);
+      break;
+    case FadeIn:
+     // TODO: configuratible max brightness (daytime/nightnime, I2C light sensor, etc)
+      setRandomFadePattern(152, randomDelay);
+      break;
+    case FadeComplete:
+      setDuration(-1);
+      break;
+  }
+}
+
+void FadeAnimation::setRandomFadePattern(int targetBrightness, int initialDelay) {
+  int durationMs = random(200, 2000);
+  int initialDelayMultiplier = random(0, 1000);
+  int* initialDelays = getRandomInitialDelays((FadePattern)random(FadePatternCount), initialDelayMultiplier);
+
+  for (int i = 0; i < NUM_TUBES; i++) {
+    setTubeFade(i, targetBrightness, durationMs, initialDelay + initialDelays[i]);
+  }
+}
+
+void FadeAnimation::setTubeFade(int tubeIndex, int targetBrightness, int durationMs, int initialDelay = 0) {
+  _tubeFadeState[tubeIndex].targetBrightness = targetBrightness;
+  _tubeFadeState[tubeIndex].stepDelay = initialDelay;
+  _tubeFadeState[tubeIndex].stepsLeft = max(1,  (int) ceil(durationMs / FadeStepDelay));
 }
 
 void FadeAnimation::initialize(Tube tubes[NUM_TUBES]) {
   Serial.println("FadeAnimation::initialize");
 
-  Animation::setDuration(20000);
-  setFade(tubes, 0);
-  _fadeProgression = FadeOut;
+  Animation::setDuration(30000);
+  _fadeProgression = FadeStart;
+  setNextProgression();
 }
 
 TickResult FadeAnimation::handleTick(Tube tubes[NUM_TUBES]) {
@@ -50,60 +85,34 @@ TickResult FadeAnimation::handleTick(Tube tubes[NUM_TUBES]) {
 
   bool update = false;
   for (int i = 0; i < NUM_TUBES; i++) {
-    if (_tubeFadeState[i].isComplete) {
+    if (_tubeFadeState[i].stepsLeft == 0) {
       continue;
     }
 
-    _tubeFadeState[i].delay--;
+    _tubeFadeState[i].stepDelay--;
 
-    if (_tubeFadeState[i].delay < 0) {
-      // set to target brightness if difference is smaller than step
-      if (abs((int)_tubeFadeState[i].targetBrightness - (int)tubes[i].Brightness) < abs(_tubeFadeState[i].step)) {
-        tubes[i].Brightness = _tubeFadeState[i].targetBrightness;
-      }
-      else {
-        tubes[i].Brightness += _tubeFadeState[i].step; // todo: overflow/underflow
-      }
-
-      // // todo: helper/util function
-      // if (tubes[i].Brightness > PWM_MAX) {
-      //   tubes[i].Brightness = PWM_MAX;
-      // }
-      // else if (tubes[i].Brightness < PWM_MIN) {
-      //   tubes[i].Brightness = PWM_MIN;
-      // }
-
-      if (tubes[i].Brightness == _tubeFadeState[i].targetBrightness) {
-        Serial.printf("Tube %d fade is complete\n", i);
-        _tubeFadeState[i].isComplete = true;
-      }
-      else {
-        _tubeFadeState[i].delay = _tubeFadeState[i].delayReset;
+    if (_tubeFadeState[i].stepDelay < 0) {
+      tubes[i].Brightness += ((int)_tubeFadeState[i].targetBrightness - (int)tubes[i].Brightness) / (int)_tubeFadeState[i].stepsLeft;
+      _tubeFadeState[i].stepsLeft--;
+      
+      if (_tubeFadeState[i].stepsLeft > 0) {
+        _tubeFadeState[i].stepDelay = FadeStepDelay;
       }
 
       update = true;
-      _tubeFadeState[i].delay = _tubeFadeState[i].delayReset;
-    }
-
-    if (update) {
-      bool shouldProgress = true;
-      for (int i = 0; i < NUM_TUBES; i++) {
-        shouldProgress &= _tubeFadeState[i].isComplete;
-      }
-
-      if (shouldProgress) {
-        Serial.printf("FadeProgression:%d->", _fadeProgression);
-        _fadeProgression = (FadeProgression)((int)_fadeProgression + 1);
-        Serial.println(_fadeProgression);
-
-        if (_fadeProgression == FadeIn) {
-          setFade(tubes, 150);
-        }
-        else if (_fadeProgression == FadeComplete) {
-          setDuration(-1);
-        }
-      }
     }
   }
+
+  if (update) {
+    bool shouldProgress = true;
+    for (int i = 0; i < NUM_TUBES; i++) {
+      shouldProgress &= _tubeFadeState[i].stepsLeft == 0;
+    }
+
+    if (shouldProgress) {
+      setNextProgression();
+    }
+  }
+
   return {false, update};
 }
--- lib/Animation/fadeAnimation.h
+++ lib/Animation/fadeAnimation.h
@@ -2,32 +2,36 @@
 #include "tubes.h"
 
 typedef enum {
-  FadeLeft,
-  FadeRight,
-  FadeRandom
-} FadeType;
+  FadeLeft = 0,
+  FadeRight = 1,
+  FadeRandom = 2,
+  FadePatternCount = 3
+} FadePattern;
 
 typedef enum {
-  FadeOut = 0,
-  FadeIn = 1,
-  FadeComplete = 2
+  FadeStart = 0,
+  FadeOut = 1,
+  FadeIn = 2,
+  FadeComplete = 3
 } FadeProgression;
 
 typedef struct {
   uint8_t targetBrightness;
-  int step; // function pointer?
-  int delayReset;
-  int delay;
-  bool isComplete;
+  int stepDelay;
+  uint8_t stepsLeft;
 } TubeFadeState;
 
 class FadeAnimation : public Animation {
   private:
-    FadeType _fadeInType;
-    FadeType _fadeOutType;
+    const int FadeStepDelay = 32; // TODO: needs to be different for different fade durations
     FadeProgression _fadeProgression;
+    int* getRandomInitialDelays(FadePattern fadeType, int fadeMultiplier);
+    int* getRandomTubeOrder();
+    void setNextProgression();
+    void setRandomFadePattern(int targetBrightness, int initialDelay = 0);
+    void setTubeFade(int tubeIndex, int targetBrightness, int durationMs, int initialDelay);
+  protected:
     TubeFadeState _tubeFadeState[NUM_TUBES]; // TODO: FadeHandler
-    void setFade(Tube tubes[NUM_TUBES], int targetBrightness);
   public:
     void initialize(Tube tubes[NUM_TUBES]) override;
     TickResult handleTick(Tube tubes[NUM_TUBES]) override;
